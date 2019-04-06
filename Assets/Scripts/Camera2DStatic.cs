3cf6b01221e65c330a070e43916fd5ef86a8ab55
hussein-albreihi <hussein.albreihi@hotmail.com>
2019-04-06 22:49:43 +0200

    init

---
+++ Assets/Scripts/Camera2DStatic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2DStatic : MonoBehaviour
+{
+    public float speed = 0f;
+    public Transform player;
+    public float damping = 0f;
+
+    bool stopCamera = false;
+    private Vector3 currentVelocity;
+    private float cameraXPosition = 0f;
+
+    void Start()
+    {
+        cameraXPosition = player.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopCamera)
+        {
+            Debug.Log("change");
+            Vector3 targetPosition = new Vector3(cameraXPosition, speed + (speed * 0.1f));
+            Vector3 newPos = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, damping);
+
+            transform.position = newPos;
+        }
+    }
+}

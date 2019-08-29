974e71bc6b417c82cac9471d7e6ec582e659583a
husse <hussein.albreihi@hotmail.com>
2019-08-29 22:34:45 +0200

    update current everything

--- Assets/Scripts/JumpButtonController.cs
+++ Assets/Scripts/JumpButtonController.cs
@@ -3,29 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class JumpButtonController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+public class JumpButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private PlayerController playerController;
 
+    private bool m_holdingDown = false;
+
     public virtual void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("iaejpmfo");
-        Debug.Log(eventData.pointerPressRaycast.gameObject.name);
-    }
-
-    public virtual void OnDrag(PointerEventData data)
-    {
-        Debug.Log("Finger is dragging");
-    }
-
-    public virtual void OnPointerUp(PointerEventData data)
-    {
-        RaycastResult result = data.pointerPressRaycast;
+        RaycastResult result = eventData.pointerPressRaycast;
         Debug.Log(result.gameObject.name);
 
         if (result.gameObject.name == "buttonHandler" || result.gameObject.name == "button" || result.gameObject.name == "JumpButton")
         {
+            m_holdingDown = true;
+        }
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        m_holdingDown = false;
+    }
+
+    void Update()
+    {
+        if (m_holdingDown)
+        {
+            playerController.HoldingJump();
             playerController.PlayerJump();
+        }
+        else
+        {
+            playerController.NotHoldingJump();
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float m_playerSpeed = 1f;                          // Sets default player speed and gets from user input
-    [SerializeField, Range(1, 100)] private float m_jumpForce = 1f;               // Sets default jumpforce and gets from user input
+    [SerializeField, Range(1, 100)] private float m_jumpForce = 1f;             // Sets default jumpforce and gets from user input
     [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
     [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-    [SerializeField] private float m_currentSpeed;                              // DEBUG ONLY, REMOVE!!!
-    [SerializeField, Range(1, 1000)] private float m_decimalValue;
-    [SerializeField] private Joystick joystick;
+    [SerializeField] private float m_maxJumpHeight;                             // Set max jump height for player 
+    [SerializeField, Range(1, 1000)] private float m_decimalValue;              // How much we divide the jump height with the forward speed
+    [SerializeField] private Joystick joystick;                                 // Load in jaoystick class from lib
+
 
     private float k_CeilingRadius = .2f;                                        // Radius of the overlap circle to determine if the player can stand up
-    private float k_GroundedRadius = .2f;                                       // Radius of the overlap circle to determine if grounded
+    private float k_GroundedRadius = .08f;                                      // Radius of the overlap circle to determine if grounded
     private bool m_FacingRight = true;                                          // For determining which way the player is currently facing
     private Rigidbody2D m_player;                                               // Gets player GameObject
     private bool m_Grounded = true;                                             // Boolean for if player is on the ground
     private float buttonTimer;
     private float directionHorizontal;
+    private float positonLastOnGround;
+    private bool jumpButtonPushed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -56,6 +59,7 @@ public class PlayerController : MonoBehaviour
             {
                 if (colliders[i].gameObject.name == "levelGround")
                 {
+                    positonLastOnGround = m_player.position.y;
                     m_Grounded = true;
                 }
                 else
@@ -77,7 +81,6 @@ public class PlayerController : MonoBehaviour
         if (m_player.velocity.x < m_playerSpeed)
         {
             m_player.AddRelativeForce(new Vector2(m_playerSpeed * Time.fixedDeltaTime, 0), ForceMode2D.Impulse);
-            m_currentSpeed = m_player.velocity.x;
             if (!m_FacingRight)
             {
                 Flip();
@@ -90,7 +93,6 @@ public class PlayerController : MonoBehaviour
         if (m_player.velocity.x > -m_playerSpeed)
         {
             m_player.AddRelativeForce(new Vector2(-(m_playerSpeed * Time.fixedDeltaTime), 0), ForceMode2D.Impulse);
-            m_currentSpeed = m_player.velocity.x;
             if (m_FacingRight)
             {
                 Flip();
@@ -100,17 +102,27 @@ public class PlayerController : MonoBehaviour
 
     public void PlayerJump()
     {
-        if (m_Grounded)
+        if ((m_player.position.y - positonLastOnGround <= m_maxJumpHeight) && jumpButtonPushed)
         {
             if (m_player.velocity.x == 0)
             {
-                m_player.AddRelativeForce(new Vector2(0, 20), ForceMode2D.Impulse);
+                m_player.AddForce(new Vector2(0, m_jumpForce * Time.fixedDeltaTime), ForceMode2D.Impulse);
             }
             else
             {
-                m_player.AddRelativeForce(new Vector2(0, Mathf.Abs(m_player.velocity.x * (m_jumpForce / m_decimalValue))), ForceMode2D.Impulse);
+                m_player.AddForce(new Vector2(0, Mathf.Abs(m_player.velocity.x * (m_jumpForce / m_decimalValue)) * Time.fixedDeltaTime), ForceMode2D.Impulse);
             }
-        }
+        } 
+    }
+    
+    public void NotHoldingJump()
+    {
+        jumpButtonPushed = false;
+    }
+
+    public void HoldingJump()
+    {
+        jumpButtonPushed = true;
     }
 
     public void Flip()
